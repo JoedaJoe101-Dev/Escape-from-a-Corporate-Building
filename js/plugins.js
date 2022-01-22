059c8221d89ce656d00f9c168022645f2eae3c32
Chiaki Gasai <97949145+ChiakiGasai@users.noreply.github.com>
2022-01-22 23:09:03 +0000

    0.21
    Removed video intro
    re-added startup logo
    modified disclaimer
    fixed a bug in a secret ending
    removed video ads (they can still be reimplemented if you care, i just couldn't be bothered purchasing a renderforest subscription)

--- js/plugins.js
+++ js/plugins.js
@@ -8,6 +8,6 @@ var $plugins =
 {"name":"AltSaveScreen","status":true,"description":"Alternative save/load screen layout.","parameters":{}},
 {"name":"ItemBook","status":true,"description":"Displays detailed statuses of items.","parameters":{"Unknown Data":"Something","Price Text":"Price","Equip Text":"Equip","Type Text":"Type"}},
 {"name":"EnemyBook","status":true,"description":"Displays detailed statuses of enemies.","parameters":{"Unknown Data":"Proably some idiot"}},
-{"name":"SRD_SkipTitleWithoutSave","status":true,"description":"This Plugin skips the title screen if there is no save file present.","parameters":{}},
+{"name":"SRD_SkipTitleWithoutSave","status":false,"description":"This Plugin skips the title screen if there is no save file present.","parameters":{}},
 {"name":"SRD_AutoUpdater","status":false,"description":"Allows developers to set up automatic updates to occur whenever a player plays the game.","parameters":{"Update Text URL":"","Initial Version":"0.90","== Version Options ==":"","Draw Version":"true","Version Format":"v%1","Version Text X":"Graphics.boxWidth - textWidth - 8","Version Text Y":"Graphics.boxHeight - textHeight - 24","Version Text Font":"GameFont","Version Text Color":"#FFFFFF","Version Text Size":"28","Version Outline Size":"3","Version Italic":"true"}}
 ];
