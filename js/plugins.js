9b3e6d4d1317593a792b02e14f652a158d5749e2
Chiaki Gasai <97949145+ChiakiGasai@users.noreply.github.com>
2022-08-28 21:45:38 +0100

    getting there slowly

--- js/plugins.js
+++ js/plugins.js
@@ -3,7 +3,7 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"logo-1","Show Custom Splash":"true","Custom Image":"logo-3","Fade Out Time":"60","Fade In Time":"60","Wait Time":"160"}},
+{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"logo-1","Show Custom Splash":"true","Custom Image":"MadeWithMv","Fade Out Time":"60","Fade In Time":"60","Wait Time":"160"}},
 {"name":"AltMenuScreen","status":false,"description":"Alternative menu screen layout.","parameters":{}},
 {"name":"AltSaveScreen","status":true,"description":"Alternative save/load screen layout.","parameters":{}},
 {"name":"ItemBook","status":true,"description":"Displays detailed statuses of items.","parameters":{"Unknown Data":"Something","Price Text":"Price","Equip Text":"Equip","Type Text":"Type"}},
